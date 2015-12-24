88b4c1aabd6bfefb681d3ed89d294e74235f1537
Toni Suominen <toni.suominen@reaktor.com>
2015-12-24 12:56:46 +0200

    Initial Commit

---
+++ HSL Nearest Departures/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HSL Nearest Departures
+//
+//  Created by Toni Suominen on 24/12/15.
+//  Copyright © 2015 Toni Suominen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HSL Nearest DeparturesTests/HSL_Nearest_DeparturesTests.swift
@@ -0,0 +1,36 @@
+//
+//  HSL_Nearest_DeparturesTests.swift
+//  HSL Nearest DeparturesTests
+//
+//  Created by Toni Suominen on 24/12/15.
+//  Copyright © 2015 Toni Suominen. All rights reserved.
+//
+
+import XCTest
+@testable import HSL_Nearest_Departures
+
+class HSL_Nearest_DeparturesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
