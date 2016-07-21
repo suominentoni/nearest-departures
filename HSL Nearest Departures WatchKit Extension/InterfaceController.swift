588b1bb5a54119790c250f518bc9b0b0ee7125aa
Toni Suominen <toni.suominen@reaktor.com>
2016-07-21 18:07:22 +0200

    Set WCSession each time Nearest Stops view activates
    This should probably fix a bug where new stop information would not be
    received by the watch after viewing next deps once and returning to
    nearest stops view.

--- HSL Nearest Departures WatchKit Extension/InterfaceController.swift
+++ HSL Nearest Departures WatchKit Extension/InterfaceController.swift
@@ -118,6 +118,7 @@ public class InterfaceController: WKInterfaceController, WCSessionDelegate {
     }
 
     override public func willActivate() {
+        session = WCSession.defaultSession()
         refreshInterface()
     }
 }
