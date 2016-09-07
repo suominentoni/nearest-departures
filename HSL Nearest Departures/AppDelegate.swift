3682c7a445c4e8947c27b3036ae4e05fcc3790e7
Toni Suominen <toni.suominen@reaktor.com>
2016-09-07 15:48:48 +0300

    Migrate fav stops with old code format to new on app start

--- HSL Nearest Departures/AppDelegate.swift
+++ HSL Nearest Departures/AppDelegate.swift
@@ -7,6 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, WCSessionDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        FavoriteStops.migrateToAgencyPrefixedCodeFormat()
         return true
     }
 
