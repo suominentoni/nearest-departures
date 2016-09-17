6561bf1c81b3ea14cbfd92c2b464606d492980d9
Toni Suominen <toni.suominen@reaktor.com>
2016-09-17 18:50:01 +0300

    Add code width constraint only once in next deps cell

--- HSL Nearest Departures/NextDeparturesTableViewController.swift
+++ HSL Nearest Departures/NextDeparturesTableViewController.swift
@@ -129,11 +129,12 @@ class NextDeparturesTableViewController: UITableViewController {
                 let destination = departure.line.destination {
 
                 cell.code.text = codeShort
-                let codeWidthConstraint = NSLayoutConstraint(item: cell.code, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 0)
-                self.hasShortCodes
-                    ? (codeWidthConstraint.constant = 55)
-                    : (codeWidthConstraint.constant = 0)
-                cell.code.addConstraint(codeWidthConstraint)
+                let codeLabelWidth: CGFloat = self.hasShortCodes ? 55 : 0
+                if(cell.codeWidthConstraint != nil) {
+                    cell.contentView.removeConstraint(cell.codeWidthConstraint!)
+                }
+                cell.codeWidthConstraint = NSLayoutConstraint(item: cell.code, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: codeLabelWidth)
+                cell.contentView.addConstraint(cell.codeWidthConstraint!)
 
                 cell.destination.text = destination
             } else {
