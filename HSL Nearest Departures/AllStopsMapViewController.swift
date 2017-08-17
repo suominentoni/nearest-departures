7ff1fcd30a4eaf2dd0babe38e0c03b8f4a175e17
Toni Suominen <toni.suominen@reaktor.com>
2017-08-17 13:57:09 +0300

    Show pins for stops nearest to center of map

--- HSL Nearest Departures/AllStopsMapViewController.swift
+++ HSL Nearest Departures/AllStopsMapViewController.swift
@@ -11,6 +11,41 @@ import UIKit
 import MapKit
 
 class AllStopsMapViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var allStopsMap: MKMapView!
+    var annotations: [MKAnnotation] = []
+
     override func viewDidLoad() {
+        allStopsMap.delegate = self
+        super.viewDidLoad()
+
+        displayStopsForCurrentRegion()
+    }
+
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        displayStopsForCurrentRegion()
+    }
+
+    fileprivate func displayStopsForCurrentRegion() {
+        let lat = self.allStopsMap.centerCoordinate.latitude
+        let lon = self.allStopsMap.centerCoordinate.longitude
+
+        HSL.nearestStopsAndDepartures(lat, lon: lon, callback: {(stops: [Stop]) in
+            let stopPins = stops.map({stop -> MKPointAnnotation in
+                let lat = CLLocationDegrees(floatLiteral: stop.lat)
+                let lon = CLLocationDegrees(floatLiteral: stop.lon)
+                let pin = MKPointAnnotation()
+                pin.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+
+                return pin
+            })
+
+            DispatchQueue.main.async {
+                self.allStopsMap.removeAnnotations(self.annotations)
+                self.annotations = stopPins
+                self.allStopsMap.addAnnotations(stopPins)
+            }
+        })
+
     }
 }
