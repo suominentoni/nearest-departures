9ec048a417614350a41ef20ef60504a9fbb39679
Toni Suominen <toni.suominen@reaktor.com>
2017-08-17 13:06:09 +0300

    Add a map view in the navigation bar

---
+++ HSL Nearest Departures/AllStopsMapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AllStopsMapsViewController.swift
+//  HSL Nearest Departures
+//
+//  Created by Toni Suominen on 17/08/2017.
+//  Copyright © 2017 Toni Suominen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class AllStopsMapViewController: UIViewController, MKMapViewDelegate {
+    override func viewDidLoad() {
+    }
+}
