45dc36af9b02fd7e98e1bfd5c9b524095efb21af
Toni Suominen <toni.suominen@reaktor.com>
2018-08-06 16:09:21 +0300

    Add GoogleMobileAds framework

--- HSL Nearest Departures/AppDelegate.swift
+++ HSL Nearest Departures/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import GoogleMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if (ProcessInfo.processInfo.arguments.contains("UITEST_ERRONEOUSDATA")) {
             FavoriteStops.add(Stop(name: "Foo", lat: 0, lon: 0, distance: "0", codeLong: "invalid", codeShort: "invalid", departures: []))
         }
+        GADMobileAds.configure(withApplicationID: "ca-app-pub-3940256099942544/2934735716")
         return true
     }
 }
