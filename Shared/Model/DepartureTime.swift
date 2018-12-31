5df23f03a0865b945cd55a5995dcd661a533f63e
Toni Suominen <toni.suominen@reaktor.com>
2018-12-31 19:15:09 +0200

    Indicate if departure time is on the next day '(+1) 12:12'

--- Shared/Model/DepartureTime.swift
+++ Shared/Model/DepartureTime.swift
@@ -15,8 +15,12 @@ extension DepartureTime {
     func toTime() -> String {
         let minutes = self / 60
         let hoursInt = minutes/60
-        let hours = String(format: "%02d", hoursInt >= 24 ? hoursInt - 24 : hoursInt)
+        let days = (self % 86400) / 60
+        let hours = String(format: "%02d", hoursInt >= 24 ? hoursInt - (24 * days) : hoursInt)
         let remainder = String(format: "%02d", minutes % 60)
-        return "\(hours):\(remainder)"
+        let plusOneDay = self >= 86400
+            ? "(+\(days)) "
+            : ""
+        return "\(plusOneDay)\(hours):\(remainder)"
     }
 }
